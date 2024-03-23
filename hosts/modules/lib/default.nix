83771fc8a66d6644d0622ba808f5ee26d8dfdbc2
threadexio <pzarganitis@gmail.com>
2024-03-23 22:52:31 +0200

    hash usbDevice for mount path

--- hosts/modules/lib/default.nix
+++ hosts/modules/lib/default.nix
@@ -22,7 +22,7 @@
     }:
     (
       let
-        usbMountPath = "/key/${usbDevice}";
+        usbMountPath = "/key/" ++ (builtins.hashString "md5" usbDevice);
         usbFsType = "vfat";
       in
       { lib, ... }:
