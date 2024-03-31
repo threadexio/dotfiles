d5db592b8da0a3771602882fdf05d9598c9d5c4a
threadexio <pzarganitis@gmail.com>
2024-03-31 13:52:02 +0300

    switch cpu governor to schedutil

--- hosts/ares/default.nix
+++ hosts/ares/default.nix
@@ -39,15 +39,12 @@ in
   services.fwupd.enable = true;
 
   powerManagement.enable = lib.mkForce true;
-  powerManagement.cpuFreqGovernor = "ondemand";
+  powerManagement.cpuFreqGovernor = "schedutil";
 
   services.power-profiles-daemon.enable = false;
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
     };
