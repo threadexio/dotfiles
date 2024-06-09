0b1bea3ae5c4a1552a54004deb999f35c4a4e42c
threadexio <pzarganitis@gmail.com>
2024-06-09 12:35:27 +0300

    add ydotool and wireshark

--- home/profiles/ares/system.nix
+++ home/profiles/ares/system.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   fileSystems."/home/kat/build" = {
     fsType = "tmpfs";
     options = [ "size=10g" "mode=700" "huge=always" "uid=kat" "gid=users" ];
@@ -8,4 +8,11 @@
     fsType = "tmpfs";
     options = [ "size=400m" "mode=700" "uid=kat" "gid=users" "noatime" "nodev" "nosuid" ];
   };
+
+  programs.ydotool.enable = true;
+  programs.wireshark.enable = true;
+
+  users.users.kat.extraGroups = [ "ydotool" "wireshark" ];
+
+  environment.systemPackages = with pkgs; [ wireshark-qt ];
 }
