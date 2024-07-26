bd754f4b033236eb1060dce73fcefd9fb7eae06a
threadexio <pzarganitis@gmail.com>
2024-07-26 11:27:22 +0300

    add thermald

--- hosts/ares/default.nix
+++ hosts/ares/default.nix
@@ -21,21 +21,26 @@
 
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
   boot.kernelParams = [ "net.ifnames=0" "intel_iommu=on" "iommu=pt" "intel_pstate=disable" ];
+  boot.kernelModules = [ "hp-wmi" ];
 
   fileSystems."/".options = [ "noatime" ];
   fileSystems."/boot".options = [ "noatime" ];
 
-  services.hardware.bolt.enable = true;
-  services.fwupd.enable = true;
-
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
   hardware.bluetooth = {
     enable = true;
     settings.General.Experimental = true;
   };
 
+  services.hardware.bolt.enable = true;
+  services.fwupd.enable = true;
+  services.fstrim.enable = true;
+
   powerManagement.enable = lib.mkForce true;
   powerManagement.cpuFreqGovernor = "ondemand";
   services.power-profiles-daemon.enable = true;
+  services.thermald.enable = true;
 
   services.flatpak.enable = true;
   hardware.ckb-next.enable = true;
--- hosts/modules/hardware/intel/default.nix
+++ hosts/modules/hardware/intel/default.nix
@@ -14,7 +14,9 @@
     ];
   };
 
+
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "iHD";
+    VDPAU_DRIVER = "va_gl";
   };
 }
