8d8807fcb7259dc9a39aaa6a6d30018575063530
threadexio <pzarganitis@gmail.com>
2024-08-19 21:19:13 +0300

    fix kdeconnect on plasma6

--- home/plasma/default.nix
+++ home/plasma/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }: {
-  services.kdeconnect.enable = true;
+  services.kdeconnect = {
+    enable = true;
+    package = pkgs.kdePackages.kdeconnect-kde;
+  };
 
   xdg.configFile = {
     "chromium/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
