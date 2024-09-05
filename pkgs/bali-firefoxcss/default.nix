4e121ef2b2be79847d3a4df09253169eca7a129d
threadexio <pzarganitis@gmail.com>
2024-09-05 12:06:40 +0300

    add bali-firefoxcss package

---
+++ pkgs/bali-firefoxcss/default.nix
@@ -0,0 +1,24 @@
+
+{ pkgs
+, stdenv
+, ...}:
+stdenv.mkDerivation {
+  pname = "bali-firefoxcss";
+  version = "0.1.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "Bali10050";
+    repo = "FirefoxCSS";
+    rev = "b0f21688f20c24b1937b3b398307e813bca6af38";
+    sha256 = "sha256-FrFbl6w/l49mrweFontBTdJSSkvnNhrsuE/MhNfOgkM=";
+  };
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/chrome
+
+    cp -vr ./chrome $out
+  '';
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,7 @@
         usbguard-utils = pkgs.callPackage ./usbguard-utils { };
         firefox-mod-blur = pkgs.callPackage ./firefox-mod-blur { };
         firefox-gnome-theme = pkgs.callPackage ./firefox-gnome-theme { };
+        bali-firefoxcxx = pkgs.callPackage ./bali-firefoxcss { };
       };
     };
 }
