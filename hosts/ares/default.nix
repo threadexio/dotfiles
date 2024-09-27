a632da2c7cd97fdc8b140a36d97e01c346da533c
threadexio <pzarganitis@gmail.com>
2024-09-27 19:19:02 +0300

    fix sound issue

--- hosts/ares/default.nix
+++ hosts/ares/default.nix
@@ -25,6 +25,9 @@
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
   boot.kernelParams = [ "net.ifnames=0" "intel_iommu=on" "iommu=pt" "intel_pstate=disable" ];
   boot.kernelModules = [ "hp-wmi" ];
+  boot.extraModprobeConfig = ''
+    options snd_intel_dspcfg dsp_driver=1
+  '';
 
   fileSystems."/".options = [ "noatime" ];
   fileSystems."/boot".options = [ "noatime" ];
