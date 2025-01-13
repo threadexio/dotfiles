0a6238dfa2c59b56dde4ae226472e97e388c49ad
threadexio <pzarganitis@gmail.com>
2025-01-13 22:51:55 +0200

    remove `.ssh/authorized_keys` because of bad mode

--- home/modules/programs/ssh/default.nix
+++ home/modules/programs/ssh/default.nix
@@ -3,16 +3,5 @@
     enable = false;
 
     compression = true;
-    matchBlocks = {
-      "github.com" = {
-        user = "git";
-        identityFile = "~/.ssh/github";
-      };
-    };
-  };
-
-  home.file.".ssh/authorized_keys" = {
-    enable = true;
-    source = ./authorized_keys;
   };
 }
