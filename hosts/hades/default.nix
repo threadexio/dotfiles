aefb96158cfad43102a287c3e293a0e23bb26822
threadexio <pzarganitis@gmail.com>
2025-01-13 22:49:12 +0200

    add wake-on-lan capabilities for `hades`

--- hosts/hades/default.nix
+++ hosts/hades/default.nix
@@ -1,5 +1,4 @@
-{ self
-, pkgs
+{ pkgs
 , lib
 , ...
 }: {
@@ -26,6 +25,24 @@
 
   powerManagement.enable = true;
   powerManagement.cpuFreqGovernor = "performance";
+
+  systemd.services.wol = {
+    description = "Wake-On-LAN";
+    requires = [ "network.target" ];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.ethtool}/bin/ethtool -s enp2s0 wol g";
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    ethtool
+  ];
 
   networking.hostName = "hades";
   system.stateVersion = "24.05";
