d7afd0cdba3912c9fea417a132e2ad5fa2b195f9
threadexio <pzarganitis@gmail.com>
2025-02-25 13:05:50 +0200

    > nix fmt

--- home/modules/dev/rust/default.nix
+++ home/modules/dev/rust/default.nix
@@ -4,18 +4,18 @@ let
   cargoConfig = pkgs.writeTextFile {
     name = "cargo-config";
     text = ''
-[build]
-target-dir = "${config.home.homeDirectory}/build"
+      [build]
+      target-dir = "${config.home.homeDirectory}/build"
 
-[target.x86_64-unknown-linux-gnu]
-linker = "${pkgs.clang}/bin/clang"
-rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]
+      [target.x86_64-unknown-linux-gnu]
+      linker = "${pkgs.clang}/bin/clang"
+      rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]
 
-[future-incompat-report]
-frequency = "always"
+      [future-incompat-report]
+      frequency = "always"
 
-[cargo-new]
-vcs = "none"
+      [cargo-new]
+      vcs = "none"
     '';
 
     destination = "/config.toml";
