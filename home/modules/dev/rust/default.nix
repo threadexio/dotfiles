55cac490a39710e855871186faa20f7bd121ff43
threadexio <pzarganitis@gmail.com>
2025-02-25 03:02:14 +0200

    set mold as the default linker for x86_64 rust

--- home/modules/dev/rust/default.nix
+++ home/modules/dev/rust/default.nix
@@ -1,9 +1,43 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+
+let
+  cargoConfig = pkgs.writeTextFile {
+    name = "cargo-config";
+    text = ''
+[build]
+target-dir = "${config.home.homeDirectory}/build"
+
+[target.x86_64-unknown-linux-gnu]
+linker = "${pkgs.clang}/bin/clang"
+rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]
+
+[future-incompat-report]
+frequency = "always"
+
+[cargo-new]
+vcs = "none"
+    '';
+
+    destination = "/config.toml";
+
+    derivationArgs = {
+      buildInputs = [
+        pkgs.clang
+        pkgs.mold
+      ];
+    };
+  };
+in
+
+{
   home.sessionPath = [
     "${config.home.homeDirectory}/.cargo/bin"
   ];
 
+  home.file.".cargo/config.toml".source = "${cargoConfig}/config.toml";
+
   home.packages = with pkgs; [
     rustup
+    cargoConfig
   ];
 }
