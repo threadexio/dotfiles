8143f01eb86922275e8b664c37cc75bb7fba9a58
threadexio <pzarganitis@gmail.com>
2025-03-10 12:04:08 +0200

    update chromium module

--- home/modules/programs/chromium/default.nix
+++ home/modules/programs/chromium/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   programs.chromium = {
     enable = true;
-    package = pkgs.chromium.override { enableWideVine = true; };
+    package = pkgs.ungoogled-chromium;
 
     commandLineArgs = [
       "--enable-logging=stderr"
@@ -13,11 +13,19 @@
       "--js-flags=--jitless"
     ];
 
+    dictionaries = with pkgs.hunspellDictsChromium; [
+      en_US
+    ];
+
     extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Privacy Badger
-      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
-      { id = "kjjgifdfplpegljdfnpmbjmkngdilmkd"; } # Link Hints
+      rec {
+        id = "ocaahdebbfolfmndjeplogmgcagdmblk";
+        version = "1.5.4.3";
+        crxPath = pkgs.fetchurl {
+          url = "https://github.com/NeverDecaf/chromium-web-store/releases/download/v${version}/Chromium.Web.Store.crx";
+          hash = "sha256-5ZO/IG1Ap7lH2HqEwgjzln4Oi5oqxNJ4wHyxoh+ZZTI=";
+        };
+      }
     ];
   };
 }
