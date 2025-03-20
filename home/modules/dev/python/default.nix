a950f4a7f45a373290e7aae9432ca523aba50e62
threadexio <pzarganitis@gmail.com>
2025-03-20 13:22:01 +0200

    add `python-lsp-server`

--- home/modules/dev/python/default.nix
+++ home/modules/dev/python/default.nix
@@ -1,6 +1,14 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    python3
+{ pkgs, ... }:
+
+let
+  python3 = pkgs.python3.withPackages (p: with p; [
+    python-lsp-server
     virtualenv
-  ];
+
+    pwntools
+  ]);
+in
+
+{
+  home.packages =  [ python3 ];
 }
