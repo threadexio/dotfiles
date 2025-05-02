57ded762cad564b3d1cbdc05b7bf856cec3f26a8
threadexio <pzarganitis@gmail.com>
2025-05-02 19:06:43 +0300

    add ssh config to hm

--- home/modules/programs/ssh/default.nix
+++ home/modules/programs/ssh/default.nix
@@ -1,7 +1,28 @@
-{ ... }: {
-  programs.ssh = {
-    enable = false;
+{ config
+, ...
+}:
 
-    compression = true;
+let
+  sshPath = "${config.home.homeDirectory}/.ssh";
+in
+
+{
+  programs.ssh = {
+    enable = true;
+
+    includes = [ "${sshPath}/config.local" ];
+
+    matchBlocks = {
+      "github.com" = {
+        user = "git";
+        identityFile = "${sshPath}/github"; 
+      };
+
+      "172.0.0.*" = {
+        user = "user";
+        extraOptions.StrictHostKeyChecking = "no";
+        extraOptions.UserKnownHostsFile = "/dev/null";
+      };
+    };
   };
 }
