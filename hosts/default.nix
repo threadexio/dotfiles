4fe77fe840b47136a20e552e9101133f673892fd
threadexio <pzarganitis@gmail.com>
2025-05-18 13:34:07 +0300

    export per-system config to flake `_module.args`

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,38 +1,52 @@
 { self
 , inputs
 , homes
+, systems
 , ...
 }:
+with builtins;
 
 let
-  nixosSystem = { modules, home }:
-    inputs.nixpkgs.lib.nixosSystem {
-      modules = [
-        { nixpkgs.overlays = [ self.overlays.packages ]; }
+  lib = inputs.nixpkgs.lib;
 
+  nixosSystem = { modules, homes ? [] }:
+    let
+      nixosHomeModules = (if length homes == 0 then [] else [
         inputs.hm.nixosModules.default
         {
           home-manager.extraSpecialArgs = { inherit self inputs; };
           home-manager.users.kat.imports = [
             { nixpkgs.overlays = [ self.overlays.packages ]; }
-          ] ++ home.modules;
+          ]
+          ++ (lib.lists.flatten (map (home: home.modules) homes));
         }
-      ] ++ modules;
+      ]);
+    in
+    inputs.nixpkgs.lib.nixosSystem {
+      modules = [
+        { nixpkgs.overlays = [ self.overlays.packages ]; }
+      ]
+      ++ modules
+      ++ nixosHomeModules;
 
       specialArgs = { inherit self inputs; };
     };
 in
 
 {
-  flake.nixosConfigurations = {
-    ares = nixosSystem {
+  _module.args.systems = {
+    ares = {
       modules = [ ./ares ];
-      home = homes.ares;
+      homes = [ homes.ares ];
     };
 
-    hades = nixosSystem {
+    hades = {
       modules = [ ./hades ];
-      home = homes.hades;
+      homes = [ homes.hades ];
     };
   };
+
+  flake.nixosConfigurations = mapAttrs
+    (_: nixosSystem)
+    systems;
 }
