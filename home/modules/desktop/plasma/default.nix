998b07ad9b62c9547f0ebd7d58a4df8a71634b06
threadexio <pzarganitis@gmail.com>
2025-07-20 20:40:59 +0300

    add vesper colors for plasma

--- home/modules/desktop/plasma/default.nix
+++ home/modules/desktop/plasma/default.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  lib,
+  ...
+}:
+
+{
   imports = [ ../. ];
 
   services.kdeconnect = {
@@ -7,8 +13,17 @@
   };
 
   xdg.configFile = {
-    "chromium/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    "chromium/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source =
+      "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
   };
+
+  xdg.dataFile = lib.mapAttrs' (
+    color-scheme: type:
+    lib.nameValuePair "color-schemes/${color-scheme}" {
+      enable = true;
+      source = ./color-schemes/${color-scheme};
+    }
+  ) (builtins.readDir ./color-schemes);
 
   home.packages = with pkgs; [
     kdePackages.konsole
