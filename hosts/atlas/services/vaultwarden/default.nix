9a65e94f8695415e9aa28e4e8d3c0c3e7a5272a7
threadexio <pzarganitis@gmail.com>
2025-11-16 22:50:08 +0200

    atlas: backup vaultwarden to google drive

--- hosts/atlas/services/vaultwarden/default.nix
+++ hosts/atlas/services/vaultwarden/default.nix
@@ -1,9 +1,15 @@
-{ pkgs
+{ config
+, pkgs
+, lib
 , btrfsDataMount
 , ...
 }:
 
 {
+  sops.secrets = {
+    "vaultwarden/backup_key".owner = "vaultwarden";
+  };
+  
   services.vaultwarden = {
     enable = true;
 
@@ -23,4 +29,58 @@
   };
 
   fileSystems."/var/lib/vaultwarden" = btrfsDataMount "@vaultwarden";
+
+  systemd.timers.vaultwarden-backup = {
+    description = "Vaultwarden Backup";
+    wantedBy = [ "timers.target" ];
+
+    timerConfig = {
+      OnCalendar = "02:00";
+      Persistent = true;
+    };
+  };
+
+  systemd.services.vaultwarden-backup = {
+    description = "Vaultwarden Backup";
+    conflicts = [ "vaultwarden.service" ];
+    after = [ "vaultwarden.service" ];
+    onSuccess = [ "vaultwarden.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = false;
+      User = "vaultwarden";
+      UMask = 0077;
+      StateDirectory = "vaultwarden-backup";
+      StateDirectoryMode = "0700";
+      WorkingDirectory = "/var/lib/vaultwarden-backup";
+      PrivateTmp = true;
+    };
+
+    environment = {
+      PATH = lib.mkForce (lib.makeBinPath (with pkgs; [
+        coreutils
+        gnutar
+        xz
+        age
+        rclone
+      ]));
+
+      RCLONE_CONFIG = "/var/lib/vaultwarden-backup/rclone.conf";
+    };
+
+    script = ''
+      tar -cJp --exclude icon_cache -C /var/lib/vaultwarden . \
+      | age -e -i ${config.sops.secrets."vaultwarden/backup_key".path} > /tmp/backup
+      rclone moveto /tmp/backup "drive:/vaultwarden/$(date +%F).tar.xz.age"
+    '';
+  };
+
+  systemd.tmpfiles.settings.vaultwarden-backup = {
+    "/var/lib/vaultwarden-backup".d = {
+      user = "vaultwarden";
+      group = "vaultwarden";
+      mode = "0700";
+    };
+  };
 }
