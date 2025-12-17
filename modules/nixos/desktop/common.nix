383d4582022dac29d74f465e593dde4a38fc17d9
threadexio <pzarganitis@gmail.com>
2025-12-17 09:12:33 +0200

    fix audio cracking

--- modules/nixos/desktop/common.nix
+++ modules/nixos/desktop/common.nix
@@ -16,6 +16,51 @@
       enable = true;
       support32Bit = true;
     };
+
+    # https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-disable-suspension.conf" ''
+        monitor.alsa.rules = [
+          {
+            matches = [
+              {
+                # Matches all sources
+                node.name = "~alsa_input.*"
+              },
+              {
+                # Matches all sinks
+                node.name = "~alsa_output.*"
+              }
+            ]
+            actions = {
+              update-props = {
+                session.suspend-timeout-seconds = 0
+              }
+            }
+          }
+        ]
+        # bluetooth devices
+        monitor.bluez.rules = [
+          {
+            matches = [
+              {
+                # Matches all sources
+                node.name = "~bluez_input.*"
+              },
+              {
+                # Matches all sinks
+                node.name = "~bluez_output.*"
+              }
+            ]
+            actions = {
+              update-props = {
+                session.suspend-timeout-seconds = 0
+              }
+            }
+          }
+        ]
+      '')
+    ];
   };
 
   fonts.packages = with pkgs; [
@@ -26,7 +71,10 @@
 
   hardware.i2c.enable = true;
   boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
-  boot.kernelModules = [ "i2c-dev" "ddcci_backlight" ];
+  boot.kernelModules = [
+    "i2c-dev"
+    "ddcci_backlight"
+  ];
   services.udev.extraRules = ''
     KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
   '';
