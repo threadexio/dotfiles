5c57a74b029c28f0298b55471828ce1c1fe8a2bf
threadexio <pzarganitis@gmail.com>
2025-12-19 10:57:24 +0200

    atlas: update wolly configuration to allow more room for wakeup

--- hosts/atlas/services/wolly/default.nix
+++ hosts/atlas/services/wolly/default.nix
@@ -24,6 +24,9 @@
         from = "0.0.0.0:10022";
         to = "10.0.0.17:22";
         wait-for = 10;
+        max-attempts = 10;
+        retry-delay = 5;
+        retry-factor = 1.1;
       }
 
       # Nix Binary Cache
@@ -31,6 +34,9 @@
         from = "0.0.0.0:15000";
         to = "10.0.0.17:5000";
         wait-for = 10;
+        max-attempts = 10;
+        retry-delay = 5;
+        retry-factor = 1.1;
       }
 
       # Minecraft Server
@@ -38,7 +44,9 @@
         from = "0.0.0.0:25565";
         to = "10.0.0.17:25565";
         wait-for = 10;
-        max-attempts = 10;
+        max-attempts = 20;
+        retry-delay = 5;
+        retry-factor = 1.5;
       }
     ];
   };
